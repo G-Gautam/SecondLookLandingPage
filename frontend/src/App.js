5880aea939fd95809e0a6a2996cd8168786605ad
Gautam Gupta <gautamgupta9298@gmail.com>
2021-03-05 04:03:18 -0500

    Added pictures

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import lockImage from './lock.png'
+import realTimeImage from './realtime.png'
+import searchImage from './search.png'
+import warningImage from './warning.png'
 
 function App() {
     return (
@@ -20,28 +24,28 @@ function App() {
                         <h2 className='feature-title'>Protected Data</h2>
                         <h3 className='feature-description'>Two factor authentication and end-to-end encryption to secure all data.</h3>
                     </div>
-                    <span className="circle"></span>
+                    <span className="circle"><img className="circle-image" src={lockImage}></img></span>
                 </div>
                 <div className='feature-container' id='s'>
                     <div className='feature-subcontainer'>
                         <h2 className='feature-title'>Real Time Notifications</h2>
                         <h3 className='feature-description'>Always stay updated to any policy updates, promotions, and charges made to your insurance.</h3>
                     </div>
-                    <span className="circle"></span>
+                    <span className="circle"><img className="circle-image" src={realTimeImage}></img></span>
                 </div>
                 <div className='feature-container' id='t'>
                     <div className='feature-subcontainer'>
                         <h2 className='feature-title'>Automated Insurance Advisor</h2>
                         <h3 className='feature-description'>Alternative recommendations and coverage gap identification to make sure you never spend out of pocket.</h3>
                     </div>
-                    <span className="circle"></span>
+                    <span className="circle"><img className="circle-image" src={searchImage}></img></span>
                 </div>
                 <div className='feature-container' id='f'>
                     <div className='feature-subcontainer'>
                         <h2 className='feature-title'>AI Invoice Anomaly Detector</h2>
                         <h3 className='feature-description'>Each invoice is checked for errors and simplified for a quick read. Never again wonder about your charges.</h3>
                     </div>
-                    <span className="circle"></span>
+                    <span className="circle"><img className="circle-image" id='warning' src={warningImage}></img></span>
                 </div>
             </div>
         </div>
