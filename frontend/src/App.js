a04699af9d7814e6042c4cf35befc98d74949305
Gautam Gupta <gautamgupta9298@gmail.com>
2021-03-05 03:34:28 -0500

    Added feature descriptions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,26 +16,30 @@ function App() {
             </div>
             <div className="container" id='second'>
                 <div className='feature-container' id='o'>
-                    <div className='feature-title-container'>
+                    <div className='feature-subcontainer'>
                         <h2 className='feature-title'>Protected Data</h2>
+                        <h3 className='feature-description'>Two factor authentication and end-to-end encryption to secure all data.</h3>
                     </div>
                     <span className="circle"></span>
                 </div>
                 <div className='feature-container' id='s'>
-                    <div className='feature-title-container'>
+                    <div className='feature-subcontainer'>
                         <h2 className='feature-title'>Real Time Notifications</h2>
+                        <h3 className='feature-description'>Always stay updated to any policy updates, promotions, and charges made to your insurance.</h3>
                     </div>
                     <span className="circle"></span>
                 </div>
                 <div className='feature-container' id='t'>
-                    <div className='feature-title-container'>
+                    <div className='feature-subcontainer'>
                         <h2 className='feature-title'>Automated Insurance Advisor</h2>
+                        <h3 className='feature-description'>Alternative recommendations and coverage gap identification to make sure you never spend out of pocket.</h3>
                     </div>
                     <span className="circle"></span>
                 </div>
                 <div className='feature-container' id='f'>
-                    <div className='feature-title-container'>
+                    <div className='feature-subcontainer'>
                         <h2 className='feature-title'>AI Invoice Anomaly Detector</h2>
+                        <h3 className='feature-description'>Each invoice is checked for errors and simplified for a quick read. Never again wonder about your charges.</h3>
                     </div>
                     <span className="circle"></span>
                 </div>
